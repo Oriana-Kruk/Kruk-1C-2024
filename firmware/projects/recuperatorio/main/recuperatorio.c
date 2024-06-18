cad750b3775153d57c7ffad595d5dadda18d02e2
Oriana-Kruk <orianafk24@gmail.com>
2024-06-18 12:49:43 -0300

    realizo la segunda tarea

--- firmware/projects/recuperatorio/main/recuperatorio.c
+++ firmware/projects/recuperatorio/main/recuperatorio.c
@@ -34,13 +34,21 @@
 #include "hc_sr04.h"
 #include "led.h" 
 #include "analog_io_mcu.h"
+#include "uart_mcu.h"
 
 
 #define TIME_PERIOD 1000000 //1 segundo
+#define TIME_PERIOD2 100000 // 100 ms
 
 #define SENSOR_TEMPERATURA CH1
+
+#define VOLTAJE_MIN 0.0
+#define VOLTAJE_MAX 3300
+#define TEMPERATURA_MIN 20.0
+#define TEMPERATURA_MAX 50.0
 /*==================[macros and definitions]=================================*/
 uint16_t distancia;
+float promedio;
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -55,7 +63,10 @@ void Notify2(void *param)
 {
 	vTaskNotifyGiveFromISR(task_handle2, pdFALSE);
 }
-
+uint16_t ConvertirVoltajeATemperatura(uint16_t voltaje)
+{
+	return TEMPERATURA_MIN + (voltaje - VOLTAJE_MIN) * (TEMPERATURA_MAX - VOLTAJE_MIN) / (VOLTAJE_MAX - VOLTAJE_MIN);
+}
 
 void medirDistancia(void *pvParameter)
 {
@@ -79,25 +90,63 @@ void medirDistancia(void *pvParameter)
 	}
 }		
 
-void medirTemeperatura(void *pvParameter)
+void medirTemperatura(void *pvParameter)
 {
+	uint16_t temperatura_mV;
+	uint16_t temperatura;
+
+	int numMediciones = 10;
+    float mediciones[numMediciones];
+	float suma = 0.0;
+
 	while (1)
 	{
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // La tarea espera en este punto hasta recibir una notificación
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+    if (distancia >=8 && distancia <=12)
+	{
+		 for(int i = 0; i < numMediciones; i++)
+		{
+		AnalogInputReadSingle(SENSOR_TEMPERATURA, &temperatura_mV); 
+		temperatura = ConvertirVoltajeATemperatura(temperatura_mV);
+
+        mediciones[i] = temperatura;
+		suma += mediciones[i];
+		float promedio = suma / numMediciones;
+	    }	
+
+	    // MESAJES POR LA UART
+		UartSendString(UART_PC, (char *) UartItoa(promedio, 10));
+        UartSendString(UART_PC, "Cº");
+        UartSendString(UART_PC, (char *) UartItoa(distancia, 10));
+        UartSendString(UART_PC, "cm\r\n");
+	 
+	}
     }
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
 
-	// configuro timer 
+	// configuro timer 1 para el control de las distancias
 	timer_config_t timer_1 = {
 		.timer = TIMER_A,
 		.period = TIME_PERIOD,
 		.func_p = Notify,
 		.param_p = NULL};
 
-	TimerInit(&timer_1); // inicializo timer
+	TimerInit(&timer_1); // inicializo timer1
+
+		timer_config_t timer_2 = {
+		// configuro timer 2 para el control de la temperatura
+		.timer = TIMER_B,
+		.period = TIME_PERIOD2,
+		.func_p = Notify2,
+		.param_p = NULL};
+
+	TimerInit(&timer_2); // inicializo timer 2
+
 	TimerStart(timer_1.timer); // para que comience el timer 1
+	TimerStart(timer_2.timer); // para que comience el timer 2
 
 	HcSr04Init(GPIO_20, GPIO_21); 
 	GPIOInit(SENSOR_TEMPERATURA, GPIO_OUTPUT);
